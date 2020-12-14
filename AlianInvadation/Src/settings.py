f464f1e4cda1276701de7e294256225ca8f84fbc
liuly <liuly@MacBook-Pro.local>
2020-12-14 18:22:18 +0800

    add settings.py

---
+++ AlianInvadation/Src/settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """
+        存储游戏设置的类型
+    """
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
